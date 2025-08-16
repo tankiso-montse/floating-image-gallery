b4cf09307f9bf45fb5daee6354416e101c8a5a55
Tankiso Montse <tmontse16@gmail.com>
2025-08-16 23:08:06 +0200

    added gallery images that will be used for floating image gallery. Moved the global css file into the src folder to keep app route for page routing

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const IMAGE_PATHS = {
+  image1: '/image-1.jpg',
+  image2: '/image-2.jpg',
+  image3: '/image-3.jpg', 
+  image4: '/image-4.jpg',
+  image5: '/image-5.jpg',
+  image6: '/image-6.jpg',
+  image7: '/image-7.jpg',
+  image8: '/image-8.jpg',
+}
