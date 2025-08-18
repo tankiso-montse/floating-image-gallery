cd5e6ace171744b1735dec0e251a77e7576308a5
Tankiso Montse <tmontse16@gmail.com>
2025-08-18 15:25:23 +0200

    wrote a seperate animate function that controls gsap animations and calls the requestAnimateFrame to recursively call the animate function to update the planes coordinates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,28 @@ export default function Home() {
   const plane3 = useRef(null);
 
   const speed = 0.1; // Controls the speed of the mouse movement. Adjust as needed.
+  let xForce = 0; 
+  let yForce = 0;
+  let requestAnimationFrameId: number | null = null;
 
 
   const manageMouseMove = (event: React.MouseEvent) => {
     // Event handler accesses the y and x movement of the current mouseMove event and using gsap we control the direction of each plane and the speed that it moves in.
     // Further reduced speed on plane2 and 3 so we get a move balanced and clean animation of a floating effect.
     const {movementX, movementY} = event;
-    gsap.set(plane1.current, {x: `+=${movementX * speed}`, y: `+=${movementY * speed}`})
-    gsap.set(plane2.current, {x: `+=${movementX * speed * 0.5}`, y: `+=${movementY * speed * 0.5}`})
-    gsap.set(plane3.current, {x: `+=${movementX * speed * 0.25}`, y: `+=${movementY * speed * 0.25} `})
+      xForce = movementX * speed; 
+      yForce = movementY * speed;
+
+    if (requestAnimationFrameId === null) {
+      requestAnimationFrameId = requestAnimationFrame(animate);
+    }
+  }
+
+  const animate = () => {
+    gsap.set(plane1.current, {x: `+=${xForce}`, y: `+=${yForce}`})
+    gsap.set(plane2.current, {x: `+=${xForce * 0.5}`, y: `+=${yForce * 0.5}`})
+    gsap.set(plane3.current, {x: `+=${xForce * 0.25}`, y: `+=${yForce * 0.25} `})
+    requestAnimationFrame(animate); // recursive function call to keep the animation going
   }
 
   return (
