c0b59647a46e064b9c8916d271fcc6ceec7a487c
Tankiso Montse <tmontse16@gmail.com>
2025-08-18 06:37:30 +0200

    fixed constant path names for images and updated code to access them. Added styles to place images across the screen in each plane

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+import Image from "next/image";
+import { useRef } from "react";
+import { IMAGE_PATHS } from "@/constants";
 
 
 export default function Home() {
+  const plane1 = useRef(null);
+  const plane2 = useRef(null);
+  const plane3 = useRef(null);
   return (
-    <div></div>
+    <main className="h-screen w-screen overflow-hidden relative">
+      <div ref={plane1} className="plane">
+        <Image src={IMAGE_PATHS.image1} alt="image" width={200} height={200} />
+        <Image src={IMAGE_PATHS.image2} alt="image" width={200} height={200} />
+        <Image src={IMAGE_PATHS.image3} alt="image" width={200} height={200} />
+      </div>
+      <div ref={plane2} className="plane">
+        <Image src={IMAGE_PATHS.image4} alt="image" width={200} height={200} />
+        <Image src={IMAGE_PATHS.image6} alt="image" width={200} height={200} />
+        <Image src={IMAGE_PATHS.image8} alt="image" width={200} height={200} />
+      </div>
+      <div ref={plane3} className="plane">
+        <Image src={IMAGE_PATHS.image5} alt="image" width={200} height={200} />
+        <Image src={IMAGE_PATHS.image7} alt="image" width={200} height={200} />
+      </div>
+    </main>
   );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 export const IMAGE_PATHS = {
-  image1: '/image-1.jpg',
+  image1: '/image-1.jpeg',
   image2: '/image-2.jpg',
   image3: '/image-3.jpg', 
   image4: '/image-4.jpg',
   image5: '/image-5.jpg',
-  image6: '/image-6.jpg',
-  image7: '/image-7.jpg',
-  image8: '/image-8.jpg',
+  image6: '/image-6.jpeg',
+  image7: '/image-7.jpeg',
+  image8: '/image-8.jpeg',
 }
