46e64f4f640662768c14df8c7b9c88661f47c371
Tankiso Montse <tmontse16@gmail.com>
2025-08-18 16:40:56 +0200

    created a component and just referenced it in the page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,11 @@
-'use client';
-import Image from "next/image";
-import { useRef } from "react";
-import { IMAGE_PATHS } from "@/constants";
-import gsap from "gsap";
-import { start } from "repl";
+import FloatingGalleryAnimation from "@/Components/FloatingGalleryAnimation";
 
 
 export default function Home() {
-  const plane1 = useRef(null); 
-  const plane2 = useRef(null);
-  const plane3 = useRef(null);
-
-  const speed = 0.1; // Controls the speed of the mouse movement. Adjust as needed.
-  let xForce = 0; 
-  let yForce = 0;
-  let requestAnimationFrameId: number | null = null;
-  const easing = 0.08;
-
-
-  const manageMouseMove = (event: React.MouseEvent) => {
-    // Event handler accesses the y and x movement of the current mouseMove event and using gsap we control the direction of each plane and the speed that it moves in.
-    // Further reduced speed on plane2 and 3 so we get a move balanced and clean animation of a floating effect.
-    const {movementX, movementY} = event;
-      xForce = movementX * speed; 
-      yForce = movementY * speed;
-
-    if (requestAnimationFrameId === null) {
-      requestAnimationFrameId = requestAnimationFrame(animate);
-    }
-  }
-
-  // Using linear interpolation to apply easing to the movement of the planes so they don't just float away immediately.
-  // This function takes a start value, an end value, and an amount (between 0 and 1) to interpolate between the two values.
-  // The closer the amount is to 0, the closer the result will be to the start value; the closer it is to 1, the closer it will be to the end value.
-  const lerp = (start: number, end: number, amount: number) => start + (end - start) * amount;
-
-  const animate = () => {
-    xForce = lerp(xForce, 0, easing); // Smoothly reduce the force to zero
-    yForce = lerp(yForce, 0, easing);
-
-    gsap.set(plane1.current, {x: `+=${xForce}`, y: `+=${yForce}`})
-    gsap.set(plane2.current, {x: `+=${xForce * 0.5}`, y: `+=${yForce * 0.5}`})
-    gsap.set(plane3.current, {x: `+=${xForce * 0.25}`, y: `+=${yForce * 0.25} `})
-
-    if (Math.abs(xForce) < 0.01) xForce = 0; // Reset xForce if it's very small
-    if (Math.abs(yForce) < 0.01) yForce = 0; // Reset yForce if it's very small
-
-    if (xForce === 0 && yForce === 0) {
-    requestAnimationFrame(animate); // recursive function call to keep the animation going
-    } else {
-      cancelAnimationFrame(requestAnimationFrameId!)
-      requestAnimationFrameId = null;
-    }
-  }
 
   return (
-    <main className="h-screen w-screen overflow-hidden relative" onMouseMove={manageMouseMove}>
-      {/* <div className="absolute left-1/2 top-5/12 transform -translate-x-1/2 -translate-y-1/2 ">
-        <h1 className="text-4xl text-center font-bold mb-8">Floating Image Gallery</h1>
-        <p className="text-center text-lg mb-4">Hover over the images to see the floating effect!</p>
-      </div> */}
-      <div ref={plane1} className="plane">
-        <Image src={IMAGE_PATHS.image1} alt="image" width={200} height={200} />
-        <Image src={IMAGE_PATHS.image2} alt="image" width={200} height={200} />
-        <Image src={IMAGE_PATHS.image3} alt="image" width={200} height={200} />
-      </div>
-      <div ref={plane2} className="plane">
-        <Image src={IMAGE_PATHS.image4} alt="image" width={200} height={200} />
-        <Image src={IMAGE_PATHS.image6} alt="image" width={200} height={200} />
-        <Image src={IMAGE_PATHS.image8} alt="image" width={200} height={200} />
-      </div>
-      <div ref={plane3} className="plane">
-        <Image src={IMAGE_PATHS.image5} alt="image" width={200} height={200} />
-        <Image src={IMAGE_PATHS.image7} alt="image" width={200} height={200} />
-      </div>
-    </main>
+    <>
+      <FloatingGalleryAnimation />
+    </>
   );
 }
